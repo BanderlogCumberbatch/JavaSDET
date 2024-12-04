d6b62a249b6b18075bc9f2afd9c18be07ec43eb0
BanderlogCumberbatch <kutlub.artur@gmail.com>
2024-12-04 15:15:34 +0400

    [redacted]

--- src/lection_4/Main.java
+++ src/lection_4/Main.java
@@ -1,7 +1,9 @@
 package lection_4;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class Main {
@@ -9,32 +11,34 @@ public class Main {
     public static void main(String[] args) {
         List<Student> students = getStudents();
 
-        List<Student> accepted = passTheSession(students);
-
         printStudents(students);
-
-        printStudents(accepted);
 
         printStudents(students, 3);
 
-        printStudents(accepted, 3);
+        students = passTheSession(students);
+
+        printStudents(students);
+
+        printStudents(students, 3);
 
     }
 
-    public static List<Student> passTheSession (List<Student> arr) {
-        List<Student> students = arr.stream()
-                .map(Student::new)
-                .filter(s -> s.score >= 3)
+    public static List<Student> passTheSession (List<Student> students) {
+        List<Student> result = students.stream()
+                .filter(s -> s.scores.stream()
+                        .mapToDouble(s1 -> s1) // преобразуем Integer в double
+                        .average()
+                        .orElse(Double.NaN) >= 3.0)
                 .toList();
 
-        students.forEach(s1 -> s1.course += 1);
+        result.forEach(s1 -> s1.course += 1);
 
-        return students;
+        return result;
     }
 
     public static void printStudents (List<Student> arr) {
         for (Student s : arr) {
-            System.out.println("Студент: " + s.name + ", Курс: " + s.course + ", Средний балл: " + s.score);
+            System.out.println("Студент: " + s.name + ", Курс: " + s.course + ", Оценки: " + s.scores); // преобразуем Integer в double .average()
         }
         System.out.println();
     }
@@ -51,12 +55,12 @@ public class Main {
     }
 
     private static List<Student> getStudents() {
-        Student student0 = new Student("Андрей", "МОАИС", 3, 2.3);
-        Student student1 = new Student("Сергей", "ПРИ", 4, 4.2);
-        Student student2 = new Student("Александр", "ПРИ", 4, 3.5);
-        Student student3 = new Student("Артур", "ПРИ", 4, 5.0);
-        Student student4 = new Student("Дмитрий", "МОАИС", 2, 3.0);
-        Student student5 = new Student("Артём", "МОАИС", 1, 2.9);
+        Student student0 = new Student("Андрей", "МОАИС", 3, Arrays.asList(2, 3 ,2, 3));
+        Student student1 = new Student("Сергей", "ПРИ", 4, Arrays.asList(5, 4 ,4, 5));
+        Student student2 = new Student("Александр", "ПРИ", 4, Arrays.asList(3, 3 ,4, 4));
+        Student student3 = new Student("Артур", "ПРИ", 4, Arrays.asList(5, 5 ,5, 5));
+        Student student4 = new Student("Дмитрий", "МОАИС", 2, Arrays.asList(3, 3 ,3, 3));
+        Student student5 = new Student("Артём", "МОАИС", 1, Arrays.asList(2, 3 ,3, 3));
 
         List<Student> students = new ArrayList<>();
         students.add(student0);
--- src/lection_4/Student.java
+++ src/lection_4/Student.java
@@ -1,21 +1,24 @@
 package lection_4;
 
+import java.util.List;
+
 public class Student {
     String name, group;
     Integer course;
-    Double score;
+    List<Integer> scores;
 
-    Student(String name, String group, Integer course, Double score) {
+
+    Student(String name, String group, Integer course, List<Integer> scores) {
         this.name = name;
         this.group = group;
         this.course = course;
-        this.score = score;
+        this.scores = scores;
     }
 
     Student(Student student) {
         this.name = student.name;
         this.group = student.group;
         this.course = student.course;
-        this.score = student.score;
+        this.scores = student.scores;
     }
 }
